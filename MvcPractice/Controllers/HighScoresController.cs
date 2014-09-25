fee4f9b069164f7a5efdc92f200c9be5248a5f7d
SeedPaths-User <lessdismissive@email.com>
2014-09-25 13:06:33 -0600

    Update

---
+++ MvcPractice/Controllers/HighScoresController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//bring in the objects
+using MvcPractice.Models;
+
+namespace MvcPractice.Controllers
+{
+    public class HighScoresController : Controller
+    {
+        //
+        // GET: /HighScores/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GuessThatNumber()
+        {
+            //set up a connection to the database
+            Models.HighScoresEntities db = new Models.HighScoresEntities();
+            //pass the list of high scores for GTN to the view
+            return View(db.HighScores.Where(x=> x.Game == "Guess That Number").OrderBy(x=> x.Score));
+        }
+
+        public ActionResult Battleship()
+        {
+            //set up a connection to the dtabase
+            Models.HighScoresEntities db = new Models.HighScoresEntities();
+            //pass th list of high scores for GTN to the view
+            return View(db.HighScores.Where(x => x.Game == "Battleship").OrderBy(x => x.Score));
+        }
+
+        public ActionResult DemonSlayerSimulator1()
+        {
+            //set up a connection to the dtabase
+            Models.HighScoresEntities db = new Models.HighScoresEntities();
+            //pass th list of high scores for GTN to the view
+            return View(db.HighScores.Where(x => x.Game == "DemonSlayerSimulator1").OrderBy(x => x.Score));
+        }
+
+        public ActionResult DemonSlayerSimulator2()
+        {
+            //set up a connection to the dtabase
+            Models.HighScoresEntities db = new Models.HighScoresEntities();
+            //pass th list of high scores for GTN to the view
+            return View(db.HighScores.Where(x => x.Game == "DemonSlayerSimulator2").OrderBy(x => x.Score));
+        }
+
+        public ActionResult Hangman()
+        {
+            //set up a connection to the dtabase
+            Models.HighScoresEntities db = new Models.HighScoresEntities();
+            //pass th list of high scores for GTN to the view
+            return View(db.HighScores.Where(x => x.Game == "Hangman").OrderBy(x => x.Score));
+        }
+
+        public ActionResult TriviaGame()
+        {
+            //set up a connection to the dtabase
+            Models.HighScoresEntities db = new Models.HighScoresEntities();
+            //pass th list of high scores for GTN to the view
+            return View(db.HighScores.Where(x => x.Game == "TriviaGame").OrderBy(x => x.Score));
+        }
+    }
+}
---
+++ MvcPractice/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Get: /Home/About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
