8f8339442bbdce71ae203482c5166b2eb0cd3eaa
SeedPaths-User <lessdismissive@email.com>
2014-10-08 13:46:32 -0600

    Updates

---
+++ october25th2014/Controllers/PatientController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace october25th2014.Controllers
+{
+    public class PatientController : Controller
+    {
+        //
+        // GET: /Patient/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ october25th2014/Models/DonorValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace october25th2014.Models
+{
+    [MetadataType(typeof(DonorValidation))]
+    public partial class Donor
+    {
+    }
+
+    public class DonorValidation
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string BloodType { get; set; }
+
+        [Required]
+        public string MedicalReport { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+        
+        [Required]
+        public string ContactNumber { get; set; }
+    }
+}
